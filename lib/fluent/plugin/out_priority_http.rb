5ff8ecd1b1afd82550485edbceff022affdfbe8c
adorechic <adorechic@gmail.com>
2014-09-03 23:29:53 +0900

    Add architype

--- lib/fluent/plugin/out_priority_http.rb
+++ lib/fluent/plugin/out_priority_http.rb
@@ -1,4 +1,24 @@
 module Fluent
   class PriorityHttpOutput < Fluent::Output
+    Fluent::Plugin.register_output('priority_http', self)
+
+    def configure(conf)
+      super
+    end
+
+    def start
+      super
+    end
+
+    def shutdown
+      super
+    end
+
+    def emit(tag, es, chain)
+      chain.next
+      es.each {|time,record|
+        $stderr.puts "OK!"
+      }
+    end
   end
 end
